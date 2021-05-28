0cc0f54a5c9bb003b58e9750af04a1c2ac519201
marcosmpob <marcosmpob@gmail.com>
2021-05-27 23:48:06 -0300

    inclusão section

---
+++ resources/views/site/layouts/basico.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <title>@yield('titulo')</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
+</head>
+
+<body>
+    @yield('conteudo')
+    @yield('rodape')
+</body>
+
+</html>
