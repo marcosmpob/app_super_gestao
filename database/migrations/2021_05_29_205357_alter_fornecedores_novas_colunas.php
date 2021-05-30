1ac81697b5ee791f52a3ea274614b19047e36f02
marcosmpob <marcosmpob@gmail.com>
2021-05-30 18:30:56 -0300

    incluindo rollback nas migrations

--- database/migrations/2021_05_29_205357_alter_fornecedores_novas_colunas.php
+++ database/migrations/2021_05_29_205357_alter_fornecedores_novas_colunas.php
@@ -26,6 +26,8 @@ class AlterFornecedoresNovasColunas extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('fornecedors', function (Blueprint $table) {
+            $table->dropColumn(['uf','email'])
+        });
     }
 }
